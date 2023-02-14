690c9fe6e809ac8d803e858207c9c295cf74fde0
NdegwaJulius <juliusndegwa2@gmail.com>
2023-02-14 06:33:32 +0300

    added the Botom Navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nb_utils/nb_utils.dart';
 
-void main() {
+AppStore appStore = AppStore();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initialize(aLocaleLanguageList: languageList());
+
+  appStore.toggleDarkMode(value: getBoolAsync(isDarkModeOnPref));
+
+  defaultRadius = 10;
+  defaultToastGravityGlobal = ToastGravity.BOTTOM;
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Observer(
+      builder: (_) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '$Banking_lbl_app_Name${!isMobile ? ' ${platformName()}' : ''}',
+        home: BankingSplash(),
+        theme: !appStore.isDarkModeOn
+            ? AppThemeData.lightTheme
+            : AppThemeData.darkTheme,
+        navigatorKey: navigatorKey,
+        scrollBehavior: SBehavior(),
+        supportedLocales: LanguageDataModel.languageLocales(),
+        localeResolutionCallback: (locale, supportedLocales) => locale,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
